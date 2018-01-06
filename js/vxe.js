b8059abc8e435982865fdc3f8214638f4839a911
Vijay Edwin <vedwin@vijays-mbp.attlocal.net>
2018-01-05 18:36:42 -0800

    tern working and more clarity on dependencies and folder structure

--- js/vxe.js
+++ js/vxe.js
@@ -1,9 +1,25 @@
 function cd (dir){
-    var HOME = process.env.HOME
-    var path_match = /(~\/)(.*)/
-    var clean_dir = dir.match(path_match)
+    var HOME = process.env.HOME;
+    var path_match = /(~\/)(.*)/;
+    var clean_dir = dir.match(path_match);
 
-    process.chdir(HOME + '/' + clean_dir[2])
+    process.chdir(HOME + '/' + clean_dir[2]);
 }
+
+function ls(){
+    return fs.readdirSync(".");
+}
+
+function pwd (){
+    return process.env.PWD ;
+}
+
+shell = require('shelljs');
+mysql = require('mysql');
+stylus = require('stylus');
+
+
+
+
 
 
